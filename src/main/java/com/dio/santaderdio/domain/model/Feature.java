d71f1e2002cbd82b5d866351738932d36fa5a51f
Felipe <felipesou94@gmail.com>
2023-07-25 06:38:37 -0300

    Create Entity | Conection H2

---
+++ src/main/java/com/dio/santaderdio/domain/model/Feature.java
@@ -0,0 +1,11 @@
+package com.dio.santaderdio.domain.model;
+
+
+import jakarta.persistence.Entity;
+
+
+@Entity(name="tb_feature")
+public class Feature extends BaseItem {
+
+
+}
---
+++ src/main/java/com/dio/santaderdio/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.dio.santaderdio.domain.repository;
+
+import com.dio.santaderdio.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository  extends JpaRepository<User, Long> {
+}
